f88a28b52870750134da6dffdf5aadb9a5846c0b
johndoknjas <jdoknjas@sfu.ca>
2026-03-04 17:15:35 -0800

    Have script run openingexplorer.

--- init-lichess-dev.sh
+++ init-lichess-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DEFAULT_LILA_DIR="/Users/john.doknjas/lichess/lila"
 LILA_WS_DIR="/Users/john.doknjas/lichess/lila-ws"
+OPENINGEXPLORER_DIR="/Users/john.doknjas/lichess/lila-openingexplorer"
 
 # If start-lila() was used, it drops the caller's cwd here.
 START_DIR_FILE="${TMPDIR:-/tmp}/lichess-start-dir"
@@ -22,6 +23,7 @@ tmux split-window -h "cd '$LILA_DIR' && ui/build -w; bash"
 tmux split-window -v "cd '$LILA_DIR' && killall redis-server && sleep 1; redis-server; bash"
 tmux select-pane -t 0
 tmux split-window -v "cd '$LILA_DIR' && mongod; bash"
+tmux split-window -h "cd '$OPENINGEXPLORER_DIR' && source ~/.zshrc && ulimit -n 131072 && EXPLORER_LOG=lila_openingexplorer=info cargo run --release -- --db-compaction-readahead --lila http://localhost:9663 --cors; bash"
 tmux split-window -h "cd '$LILA_WS_DIR' && source ~/.zshrc && sbt run -Dcsrf.origin=http://localhost:9663; bash"
 tmux swap-pane -s 1 -t 2
 tmux -2 attach-session -d
