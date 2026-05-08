caf174eb21cbee07b13d89ec78e84c421ffc456c
johndoknjas <jdoknjas@sfu.ca>
2026-05-07 18:15:08 -0700

    Some modifications.

--- init-lichess-dev.sh
+++ init-lichess-dev.sh
@@ -20,12 +20,14 @@ tmux new-session -d -s my_session "cd '$LILA_DIR' && source ~/.bashrc && echo 'C
 tmux setw remain-on-exit on
 
 tmux split-window -h "cd '$LILA_DIR' && ui/build -w; bash"
-tmux split-window -v "cd '$LILA_DIR' && pkill redis-server || true; sleep 1; redis-server; bash"
+tmux split-window -v "cd '$LILA_DIR' && redis-cli ping || redis-server; bash"
+tmux resize-pane -x 10
 
 tmux select-pane -t 0
-tmux split-window -v "cd '$LILA_DIR' && mongod; bash"
+tmux split-window -v "cd '$LILA_DIR' && pgrep -x mongod >/dev/null || { sudo rm -f /tmp/mongodb-27017.sock && mongod; }; bash"
 
 tmux split-window -h "cd '$OPENINGEXPLORER_DIR' && source ~/.bashrc && ulimit -n 131072 && EXPLORER_LOG=lila_openingexplorer=info ./target/release/lila-openingexplorer --db-compaction-readahead --lila http://localhost:9663 --cors; bash"
+tmux resize-pane -L 30
 
 tmux split-window -h "cd '$LILA_WS_DIR' && source ~/.bashrc && sbt run -Dcsrf.origin=http://localhost:9663; bash"
 
